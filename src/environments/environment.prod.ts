0f3e7b681940524a4317bd4d30425d17be0f6462
Renan <renan_sacavalcante@hotmail.com>
2020-06-04 18:13:55 -0300

    firebase config prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCOZczcvASdMEmQXB1TZL2LlKph0WELAoU",
+    authDomain: "crud-angular-cfd53.firebaseapp.com",
+    databaseURL: "https://crud-angular-cfd53.firebaseio.com",
+    projectId: "crud-angular-cfd53",
+    storageBucket: "crud-angular-cfd53.appspot.com",
+    messagingSenderId: "350259780818",
+    appId: "1:350259780818:web:d00fd2022e1aa1a8df610b",
+    measurementId: "G-NRDLVQ872W"
+  }
 };
